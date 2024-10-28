a384b7bd574b2e0f466fd297e412248e8d46a1dc
Thomas Campbell <tom@tcam.cloud>
2024-10-28 14:08:46 -0500

    Initial commit, coming soon page

--- data/data.ts
+++ data/data.ts
@@ -1,25 +1,25 @@
 const data = {
-  sitename: "Comming Soon Template",
-  sitetagline: "Comming Soon Template Tagline",
+  sitename: "Bank of Andy - Coming Soon",
+  sitetagline: "Memecoin Banking on BLAST NETWORK",
   sitelogo: "",
-  title: "Comming Soon",
+  title: "Bank of Andy - Coming Soon",
   description:
     "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
   newsletterheading: "Stay tuned for our live updates!",
-  copyrightText: "Copyright © 2024 | Roc Creative Studio",
+  copyrightText: "Copyright © 2024 | Prosperity Labs, LLC",
   socialIconsHeading: "Follow Us 📣",
   socialIcons: [
     {
       icon: "facebook",
-      link: "https://www.facebook.com/sandeep.baikare/",
+      link: "https://www.facebook.com/web3thomasc/",
     },
     {
       icon: "twitter",
-      link: "https://twitter.com/BaikareSandeep",
+      link: "https://twitter.com/web3thomasc",
     },
     {
       icon: "linkedIn",
-      link: "https://www.linkedin.com/in/sandip-baikare/",
+      link: "https://www.linkedin.com/in/web3thomasc/",
     },
   ],
 };

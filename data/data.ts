eda0ad6e5b6d67586a55a0f31d72213ae030bb1a
Thomas Campbell <tom@tcam.cloud>
2024-10-28 16:40:49 -0500

    content updates

--- data/data.ts
+++ data/data.ts
@@ -6,7 +6,7 @@ const data = {
   description:
     "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
   newsletterheading: "Stay tuned for our live updates!",
-  copyrightText: "Copyright © 2024 | Bank of Andy (not a real company)",
+  copyrightText: "Memecoin banking for the people, by the people",
   socialIconsHeading: "Follow Us 📣",
   socialIcons: [
     {
